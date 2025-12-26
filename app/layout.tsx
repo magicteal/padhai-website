ac130a0121febe49af87fa0799497cdfd312c6c9
Arshad <marshadkhn89@gmail.com>
2025-12-26 18:03:58 +0530

    feat: Add Google Analytics integration for enhanced tracking and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import ConditionalNavbar from "../components/ConditionalNavbar";
 import ConditionalFooter from "../components/ConditionalFooter";
@@ -34,6 +35,19 @@ export default function RootLayout({
         <ConditionalFooter />
         <FloatingWhatsAppCTA />
         <LeadCapturePopup />
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-19CRNPTLP5"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-19CRNPTLP5');
+          `}
+        </Script>
       </body>
     </html>
   );
